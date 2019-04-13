0f593caf196711d7bea926eb1842614b2ac1720f
artik292 <artik292@gmail.com>
2019-04-13 15:58:39 +0300

    Visual update

--- virtual_page/file_show.php
+++ virtual_page/file_show.php
@@ -4,6 +4,11 @@ $vir->set(function($vir) use($db,$blobClient,$folder,$app,$is_admin){
 
     $file = new File($db);
     $file->load($_SESSION['file_id']);
+
+    $columns = $vir->add('Columns');
+    $col_0 = $columns->addColumn(15);   // VISUAL FIX
+    $col_1 = $columns->addColumn(1);    // VISUAL FIX
+
     if ($file['MetaIsImage']) {
         $file_image = $file['Link'];
               if (($_SESSION['user_id'] == $folder['account_id']) OR ($is_admin)) {
@@ -30,10 +35,26 @@ $vir->set(function($vir) use($db,$blobClient,$folder,$app,$is_admin){
     $_SESSION['file_name'] = $file['MetaName'];
     return new \atk4\ui\jsModal('Rename',$virtual);
     });
+
+    /**
+      VISUAL PART
+    **/
+
     $vir->add(['Image',$file_image,'medium centered']);
     $vir->add(['Header',$file['MetaName'],'big centered']);
     $vir->add(['ui'=>'divider']);
 
+    $columns = $vir->add('Columns');
+    $col_0 = $columns->addColumn(5);
+    $col_1 = $columns->addColumn(7);
+    $col_2 = $columns->addColumn(4);
+
+    /**
+      DOWNLOAD AND DELETE FILE
+    **/
+
+    $col_0->add(['Button','Download','big green','iconRight'=>'download'])->link($file['Link']);
+    
     if (($_SESSION['user_id'] == $folder['account_id']) OR ($is_admin)) {
             if ($file['MetaIsImage']) {
                 $delete_name = 'Delete image';
@@ -41,7 +62,7 @@ $vir->set(function($vir) use($db,$blobClient,$folder,$app,$is_admin){
                 $delete_name = 'Delete file';
             }
             require 'virtual_page/check_delete_file.php';
-            $vir->add(['Button',$delete_name,'red','icon'=>'trash alternate'])->on('click', new \atk4\ui\jsModal('Are you sure?',$new_vir));
+            $col_2->add(['Button',$delete_name,'red','icon'=>'trash alternate'])->on('click', new \atk4\ui\jsModal('Are you sure?',$new_vir));
     }
 
     return 1;
