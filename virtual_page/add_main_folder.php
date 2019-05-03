73ed961e169f9f2b79f52e4aee36da87e69e81cc
artik292 <artik292@gmail.com>
2019-05-03 21:58:05 +0300

    empty check

--- virtual_page/add_main_folder.php
+++ virtual_page/add_main_folder.php
@@ -1,13 +1,18 @@
-<?php 
+<?php
 
 $vir = $app->add('VirtualPage');
 $vir->set(function($vir) use ($db) {
             $form = $vir->add('Form');
             $form->setModel(new Folder($db),['name']);
             $form->onSubmit(function($form) {
-                $form->model['account_id'] = $_SESSION['user_id'];
-                $form->model['Is_SubFolder'] = 0;
-                $form->model->save();
-                return [$form->success('Folder added!'),new \atk4\ui\jsExpression('document.location="index.php"')];
+                //if (preg_match('/^[a-zA-Z0-9_]{1,}$', $form->model['name'])) { //regular expression, need to know more 
+                if ($form->model['name'] !== '') {
+                  $form->model['account_id'] = $_SESSION['user_id'];
+                  $form->model['Is_SubFolder'] = 0;
+                  $form->model->save();
+                  return [$form->success('Folder added!'),new \atk4\ui\jsExpression('document.location="index.php"')];
+                } else {
+                  return new atk4\ui\jsNotify(['content' => "Folder name can't be empty!", 'color' => 'red']);
+                }
     });
 });
