0e7ca16ef7ebb4cb60580430f33376be023ff719
artik292 <artik292@gmail.com>
2019-05-10 14:40:44 +0300

    DataBase update
    Link and Icon for File

--- DataBase/azure.sql
+++ DataBase/azure.sql
@@ -1,168 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Время создания: Фев 22 2019 г., 16:24
--- Версия сервера: 10.1.30-MariaDB
--- Версия PHP: 7.2.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+# ************************************************************
+# Sequel Pro SQL dump
+# Версия 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Адрес: eu-cdbr-west-02.cleardb.net (MySQL 5.6.43-log)
+# Схема: heroku_0ba6ba49c3a4171
+# Время создания: 2019-05-10 11:39:22 +0000
+# ************************************************************
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- База данных: `azure`
---
 
--- --------------------------------------------------------
+# Дамп таблицы account
+# ------------------------------------------------------------
 
---
--- Структура таблицы `account`
---
+DROP TABLE IF EXISTS `account`;
 
 CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Email` varchar(90) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `Password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_access` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `file`
---
+
+# Дамп таблицы file
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `file`;
 
 CREATE TABLE `file` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ContainerName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `MetaName` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `MetaType` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `DateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `MetaIsImage` tinyint(1) NOT NULL,
+  `MetaType` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `MetaSize` int(20) NOT NULL,
-  `folder_id` int(11) NOT NULL
+  `MetaIsImage` tinyint(1) NOT NULL,
+  `Link` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Icon` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `folder_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder_id` (`folder_id`),
+  CONSTRAINT `file_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `folder`
---
+
+# Дамп таблицы folder
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `folder`;
 
 CREATE TABLE `folder` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Image` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `DateCreated` datetime DEFAULT CURRENT_TIMESTAMP,
   `DateModify` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Amount` int(10) UNSIGNED ZEROFILL DEFAULT NULL,
-  `Is_SubFolder` tinyint(1) NOT NULL DEFAULT '0',
-  `account_id` int(11) NOT NULL
+  `Amount` int(10) unsigned zerofill DEFAULT NULL,
+  `Is_SubFolder` binary(1) DEFAULT NULL,
+  `account_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `folder_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `in_folder`
---
+
+# Дамп таблицы in_folder
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `in_folder`;
 
 CREATE TABLE `in_folder` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `child_folder_id` int(11) DEFAULT NULL,
-  `folder_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `folder_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder_id` (`folder_id`),
+  CONSTRAINT `in_folder_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Индексы сохранённых таблиц
---
 
---
--- Индексы таблицы `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `file`
---
-ALTER TABLE `file`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `folder_id` (`folder_id`);
 
---
--- Индексы таблицы `folder`
---
-ALTER TABLE `folder`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `account_id` (`account_id`);
-
---
--- Индексы таблицы `in_folder`
---
-ALTER TABLE `in_folder`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `parent_folder_id` (`folder_id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `file`
---
-ALTER TABLE `file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `folder`
---
-ALTER TABLE `folder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `in_folder`
---
-ALTER TABLE `in_folder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `file`
---
-ALTER TABLE `file`
-  ADD CONSTRAINT `file_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `folder`
---
-ALTER TABLE `folder`
-  ADD CONSTRAINT `folder_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `in_folder`
---
-ALTER TABLE `in_folder`
-  ADD CONSTRAINT `in_folder_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`);
-COMMIT;
-
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
