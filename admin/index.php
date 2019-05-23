52e21df800d29b6dea1f05c49b0a5855ea210b5f
artik292 <artik292@gmail.com>
2019-05-23 20:44:13 +0300

    admin page with main info

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,63 @@ $app->initLayout('Admin');
 
 require 'layout.php';
 
-$CRUD = $app->add(['CRUD']);
-$CRUD->setModel(new Account($db));
-$CRUD->addQuickSearch(['name','Email']);
-$CRUD->addDecorator('name', new \atk4\ui\TableColumn\Link('re.php?id={$id}&way=usrtofol'));
+$basic_info = new Basic_info($db);
+$basic_info->load(1);
+
+$amount_of_users = $app->add(['View', 'template' => new \atk4\ui\Template('<div id="{$_id}" class="ui statistic">
+    <div class="label">
+      Amount of users:
+    </div>
+    <div class="value">
+      <i class="users icon"></i> {$num}
+    </div>
+  </div>')]);
+$amount_of_users->template->set('num', $basic_info['amount_of_users']);
+
+
+$total_memory_in_use = $app->add(['View', 'template' => new \atk4\ui\Template('<div id="{$_id}" class="ui statistic">
+    <div class="label">
+      Total memory in use:
+    </div>
+    <div class="value">
+       {$memory} {$letter}
+    </div>
+  </div>')]);
+
+  $memory = $basic_info['total_memory_in_use'];
+  $memory_letter = 'B';
+if (($memory/1024) > 1){
+    $memory = $memory/1024;
+    $memory_letter = 'KB';
+}
+
+if (($memory/1024) > 1){
+    $memory = $memory/1024;
+    $memory_letter = 'MB';
+}
+
+if (($memory/1024) > 1){
+    $memory = $memory/1024;
+    $memory_letter = 'GB';
+}
+
+if (($memory/1024) > 1){
+    $memory = $memory/1024;
+    $memory_letter = 'TB';
+}
+
+$memory = round($memory);
+
+$total_memory_in_use->template->set('memory', $memory);
+$total_memory_in_use->template->set('letter', $memory_letter);
+
+
+$amount_of_visitors = $app->add(['View', 'template' => new \atk4\ui\Template('<div id="{$_id}" class="ui statistic">
+    <div class="label">
+      Amount of visitors:
+    </div>
+    <div class="value">
+       <i class="eye icon"></i> {$num}
+    </div>
+  </div>')]);
+$amount_of_visitors->template->set('num', $basic_info['amount_of_visitors']);
--- admin/layout.php
+++ admin/layout.php
@@ -1,7 +1,8 @@
 <?php
 
 //$app->layout->leftMenu->addItem(['Main page', 'icon'=>'dashboard'],['main']);
-$app->layout->leftMenu->addItem(['Users', 'icon'=>'users'],['index']);
+$app->layout->leftMenu->addItem(['Main info','icon'=>'dashboard'],['index']);
+$app->layout->leftMenu->addItem(['Users', 'icon'=>'users'],['users']);
 $app->layout->leftMenu->addItem(['Folders', 'icon'=>'folder open outline'],['folders']);
 $app->layout->leftMenu->addItem(['Files', 'icon'=>'file alternate outline'],['files']);
 $app->layout->leftMenu->addItem(['Exit', 'icon'=>'sign out'],['logout_admin']);
